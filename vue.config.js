8183f90aa5edcdda86d0c4afc694d03b46d0efb6
Conor Woods <conorw@gmail.com>
2021-02-18 21:04:52 +0000

    dynamic install image

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 let sav = ''
-for (const e in process.env){
-  if ((/VUE_APP_STYLE/i).test(e)){
+for (const e in process.env) {
+  if (/VUE_APP_STYLE/i.test(e)) {
     sav += `$${e}: ${process.env[e]}\n`
   }
 }
@@ -11,9 +11,28 @@ module.exports = {
   pwa: {
     name: process.env.VUE_APP_NAME,
     desscription: process.env.VUE_APP_DESC,
+    manifestOptions: {
+      icons: [
+        {
+          src: `img/bots/${process.env.VUE_APP_NAME.toLowerCase()}_positive.svg`,
+          sizes: '192x192',
+          type: 'image/svg',
+          purpose: 'maskable any'
+        },
+        {
+          src: `img/bots/${process.env.VUE_APP_NAME.toLowerCase()}_positive.svg`,
+          sizes: '512x512',
+          type: 'image/svg',
+          purpose: 'maskable any'
+        }
+      ]
+    },
     iconPaths: {
       favicon32: `img/bots/${process.env.VUE_APP_NAME.toLowerCase()}_positive.svg`,
-      favicon16: `img/bots/${process.env.VUE_APP_NAME.toLowerCase()}_positive.svg`
+      favicon16: `img/bots/${process.env.VUE_APP_NAME.toLowerCase()}_positive.svg`,
+      appleTouchIcon: `img/bots/${process.env.VUE_APP_NAME.toLowerCase()}_positive.svg`,
+      maskIcon: `img/bots/${process.env.VUE_APP_NAME.toLowerCase()}_positive.svg`,
+      msTileImage: `img/bots/${process.env.VUE_APP_NAME.toLowerCase()}_positive.svg`
     }
   },
   css: {
